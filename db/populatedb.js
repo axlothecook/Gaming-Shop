9847f1dd43c0bbd23805ce20ac7585330eb42d27
axlothecook <alliston.ruby123@gmail.com>
2026-02-09 21:16:29 +0100

    adding changes

--- db/populatedb.js
+++ db/populatedb.js
@@ -2,50 +2,42 @@ const { Client } = require('pg');
 require('dotenv').config();
 
 const SQL = `
-CREATE TABLE IF NOT EXISTS products (
-    product_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR (255),
-    genre_id text [],
-    developer_id text [],
-    description VARCHAR (255),
-    price integer
+CREATE TABLE IF NOT EXISTS GAMES (
+  game_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR (255),
+  price integer,
+  description VARCHAR (255),
+  rating integer,
+  url VARCHAR (255),
+  isDefault boolean
 );
 
-CREATE TABLE IF NOT EXISTS genres (
-    genre_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    action VARCHAR (255),
-    adventure VARCHAR (255),
-    fighting VARCHAR (255),
-    platform VARCHAR (255),
-    puzzle VARCHAR (255),
-    racing VARCHAR (255),
-    role-playing VARCHAR (255),
-    shooter VARCHAR (255),
-    simulation VARCHAR (255),
-    sports VARCHAR (255),
-    strategy VARCHAR (255),
-    other VARCHAR (255)
+CREATE TABLE IF NOT EXISTS GENRES (
+  genre_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR (255),
+  url VARCHAR (255),
+  checked boolean,
+  isDefault boolean
 );
 
-CREATE TABLE IF NOT EXISTS developers (
-    developer_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    activision-blizzard,
-    blizzard-entertainment
-    bethesda-softworks,
-    capcom,
-    cd-project-red,
-    electronic-arts,
-    epic-games
-    kojima-production
-    namco-bandai,
-    microsoft-gaming,
-    sega,
-    sony,
-    square-enix,
-    riot-games,
-    nintendo,
-    take-two,
-    ubisoft
+CREATE TABLE IF NOT EXISTS DEVELOPERS (
+  developer_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR (255),
+  url VARCHAR (255),
+  checked boolean,
+  isDefault boolean
+);
+
+CREATE TABLE GAMES_GENRES (
+  game_id INTEGER references GAMES(game_id),
+  genre_id INTEGER references GENRES(genre_id),
+  primary key(game_id, genre_id)
+);
+
+CREATE TABLE GAMES_DEVELOPERS (
+  game_id INTEGER references GAMES(game_id),
+  developer_id INTEGER references DEVELOPERS(developer_id),
+  primary key(game_id, developer_id)
 );
 `;
 
